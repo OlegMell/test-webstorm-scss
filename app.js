39f56b953b40accad443052a9254462a8c3f6206
Oleg Melnik <oleg.mel123@gmail.com>
2020-01-25 10:28:45 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+class Person{
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const person = new Person("Вася", 18);
+
+function logPerson(person) {
+    if (!isNaN(person)){
+        
+    }
+
+}
+
+
+const elementById = document.getElementById("div");
+
